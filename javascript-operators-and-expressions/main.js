53eb34cc76ababb977981141010514692b3b666d
Mrkalen <mrkalen@hotmail.com>
2020-10-12 15:27:15 -0700

    completed javascript-operators-and-expressions Kalen Cobb

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-console */
+var width = 100;
+var height = 50;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 25;
+var payment = 26;
+var change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 70;
+var midterm = 80;
+var final = 90;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Kalen';
+var lastName = 'Cobb';
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 5;
+var isAcidic = pH < 7;
+
+console.log('vale of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headcount = 500;
+var isSparta = headcount === 300;
+
+console.log('calue of isSparta:', isSparta);
+console.log('typeOf isSparta:', typeof isSparta);
+
+var motto = fullName + ' ' + 'is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeOf motto:', typeof motto);
