e1ae4d9ce3d499c3bc1732ab7a0aef9c2a63ad73
Mrkalen <mrkalen@hotmail.com>
2020-10-13 11:18:12 -0700

    updated javascript-data-model Kalen Cobb

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -7,10 +7,10 @@ var orderHistory = [
     shippedTo: 'JS Masher',
     orderNumber: 11439416898772232,
     dateDelivered: 'Aug 8, 2020',
-    itemName: 'JavaScript for impatient programmers',
-    itemAuthor: 'Rauschmayer, Dr. Axel',
-    returnWindow: 'Sep 7, 2020',
-    price: 31.55
+    itemName: ['JavaScript for impatient programmers'],
+    itemAuthor: ['Rauschmayer, Dr. Axel'],
+    returnWindow: ['Sep 7, 2020'],
+    price: [31.55]
   },
   {
     orderPlaced: 'July 19, 2020',
@@ -18,10 +18,10 @@ var orderHistory = [
     shippedTo: 'JS Masher',
     orderNumber: 11399842681280257,
     dateDelivered: 'Jul 20, 2020',
-    itemName: 'The Timeless Way of Building',
-    itemAuthor: 'Alexander, Christopher',
-    returnWindow: 'Aug 19, 2020',
-    price: 41.33
+    itemName: ['The Timeless Way of Building'],
+    itemAuthor: ['Alexander, Christopher'],
+    returnWindow: ['Aug 19, 2020'],
+    price: [41.33]
   },
   {
     orderPlaced: 'July 4, 2020',
@@ -29,10 +29,10 @@ var orderHistory = [
     shippedTo: 'JS Masher',
     orderNumber: 11428755579059409,
     dateDelivered: 'July 7, 2020',
-    itemName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-    itemAuthor: null,
-    returnWindow: 'Aug 5, 2020',
-    price: 15.98
+    itemName: ['Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter'],
+    itemAuthor: [null],
+    returnWindow: ['Aug 5, 2020'],
+    price: [15.98]
   },
   {
     orderPlaced: 'July 3, 2020',
