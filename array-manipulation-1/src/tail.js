c4f349f06a45300b0ddb7d718c80925096df6446
Mrkalen <mrkalen@hotmail.com>
2020-10-16 16:17:42 -0700

    updated array-manipulation-1 Kalen Cobb

--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-unused-vars */
 function tail(array) {
   var afterFirst = [];
-  for (var i = 0; i < array.length; i++) {
-    if (i !== 0) {
-      afterFirst.push(array[i]);
-    }
+  for (var i = 1; i < array.length; i++) {
+    afterFirst.push(array[i]);
   }
   return afterFirst;
 }
