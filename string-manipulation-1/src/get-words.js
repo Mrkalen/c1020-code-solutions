4234bc270ff29d54d404c71ef0429ac5db6f246c
Mrkalen <mrkalen@hotmail.com>
2020-10-17 13:56:15 -0700

    in progress string-manipulation-1 reverseWord Kalen Cobb

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,1 +1,9 @@
 /* eslint-disable no-unused-vars */
+function getWords(string) {
+  if (string === '') {
+    return [];
+  } else {
+    string = string.split(' ');
+    return string;
+  }
+}
--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-unused-vars */
+function reverseWord(word) {
+  var reversed = [];
+  for (var i = word.length; i > 0; i--) {
+    reversed.push(word[i - 1]);
+  }
+  return reversed;
+}
