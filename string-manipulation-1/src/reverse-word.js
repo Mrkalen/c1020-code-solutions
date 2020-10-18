7d268479b242dbedf9fb5f184864eba10064f4b2
Mrkalen <mrkalen@hotmail.com>
2020-10-18 13:56:16 -0700

    completed string-manipulation-1 Kalen Cobb

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-unused-vars */
 function reverseWord(word) {
+  var reversedStrings = [];
   var reversed = [];
-  for (var i = word.length; i > 0; i--) {
-    reversed.push(word[i - 1]);
+  for (var i = word.length - 1; i >= 0; i--) {
+    reversedStrings.push(word[i]);
+  }
+  for (var j = 0; j < reversedStrings.length; j++) {
+    reversed += reversedStrings[j];
   }
   return reversed;
 }
