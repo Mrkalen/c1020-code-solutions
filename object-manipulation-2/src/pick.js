d1a098d9a6bd23683e0e56af1665faf14a170c02
Mrkalen <mrkalen@hotmail.com>
2020-10-20 10:27:34 -0700

    in progress object-manipulation-2 Kalen Cobb

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -6,12 +6,10 @@ function pick(source, keys) {
   for (var property in source) {
     properties.push(property);
   }
-  for (var key in source) {
-    values.push(source[property]);
+  for (var value in source) {
+    values.push(source[value]);
   }
   for (var i = 0; i < properties.length; i++) {
-    if (keys[i] === source) {
-      picked[properties] = values;
-    }
+    picked = source[keys[i]];
   }
 }
