5febb4ee45b70300856585441b174f9c22e39564
Mrkalen <mrkalen@hotmail.com>
2020-10-20 12:52:45 -0700

    updated javascript-light-bulb Kalen Cobb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,8 +1,11 @@
+var isOnOff = 0;
+
 var $bulb = document.querySelector('.bulb');
 var $container = document.querySelector('.container');
 
 $bulb.addEventListener('click', function onOff(event) {
-  if ($bulb.className === 'bulb on' && $container.className === 'container on') {
+  isOnOff++;
+  if (isOnOff % 2) {
     $bulb.className = 'bulb off';
     $container.className = 'container off';
   } else {
