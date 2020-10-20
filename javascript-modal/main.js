ecbb4e30d67a3f21367783d3a32c1f3d06f55d81
Mrkalen <mrkalen@hotmail.com>
2020-10-20 16:51:56 -0700

    updated javascript-modal Kalen Cobb

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,24 +1,13 @@
-var isOff = false;
 
 var $openModal = document.querySelector('.open-button');
 var $modalContainer = document.querySelector('.modal-container');
 
 $openModal.addEventListener('click', function popup(event) {
-  isOff = !isOff;
-  if (isOff) {
-    $modalContainer.className = 'modal-container';
-  }
-  isOff = !isOff;
+  $modalContainer.className = 'modal-container';
 });
-
-var isOn = false;
 
 var $modalButton = document.querySelector('.modal-button');
 
 $modalButton.addEventListener('click', function closePopup(event) {
-  isOn = !isOn;
-  if (isOn) {
-    $modalContainer.className = 'modal-container off';
-  }
-  isOn = !isOn;
+  $modalContainer.className = 'modal-container off';
 });
