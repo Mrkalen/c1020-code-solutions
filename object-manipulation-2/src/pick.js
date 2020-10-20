022dec9c74b4bf6bdaa08683f0246293ebe943ed
Mrkalen <mrkalen@hotmail.com>
2020-10-20 08:10:36 -0700

    in progress object-manipulation-2 pick Kalen Cobb

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,7 +1,17 @@
 /* eslint-disable no-unused-vars */
 function pick(source, keys) {
   var properties = [];
-  for (const property in source) {
+  var values = [];
+  var picked = {};
+  for (var property in source) {
     properties.push(property);
+  }
+  for (var key in source) {
+    values.push(source[property]);
+  }
+  for (var i = 0; i < properties.length; i++) {
+    if (keys[i] === source) {
+      picked[properties] = values;
+    }
   }
 }
