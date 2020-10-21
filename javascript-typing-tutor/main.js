857f3e513d803b9c21a16ce5e49551884321d492
Mrkalen <mrkalen@hotmail.com>
2020-10-20 18:03:20 -0700

    in progress javascript-typing-tutor Kalen Cobb

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,9 @@
+var count = 0;
+
+var $keypress = document.querySelectorAll('span');
+
+$keypress.addEventListener('keydown', function pressedKey(event) {
+  for (var i = 0; i < myNodeList.length; i++) {
+    const item = myNodeList[i];
+  }
+});
