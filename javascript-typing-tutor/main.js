77a9cabbe347b10f7c31c0d991fc10c183fbc6c6
Mrkalen <mrkalen@hotmail.com>
2020-10-22 18:03:39 -0700

    in progress fix underline for space javascript-typing-tutor Kalen Cobb

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,3 +1,5 @@
+var count = 0;
+
 var $keyPress = document.querySelectorAll('span');
 
 var $focus = document.querySelector('body');
@@ -7,12 +9,19 @@ $focus.addEventListener('keydown', pressedKey);
 var i = 0;
 
 function pressedKey(event) {
+  console.log('typeOf', typeof event.key);
+  console.log('typeOf', typeof $keyPress[i].textContent);
   console.log('event.key:', event.key);
   console.log('keyPress:', $keyPress[i].textContent);
-  if ($keyPress[i].textContent === event.key) {
+  if (event.key === $keyPress[i].textContent) {
     $keyPress[i].setAttribute('class', 'correct');
     i += 1;
+    $keyPress[i].setAttribute('class', 'underline');
   } else {
-    $keyPress[i].setAttribute('class', 'wrong');
+    $keyPress[i].setAttribute('class', 'wrong underline');
+  }
+  if (event.key) {
+    count++;
+    console.log('count:', count);
   }
 }
