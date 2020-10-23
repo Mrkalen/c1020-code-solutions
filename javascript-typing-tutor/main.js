911aa31d31166c01d745afe56f17ffcecbfcddc3
Mrkalen <mrkalen@hotmail.com>
2020-10-23 10:04:50 -0700

    in progress still cant figure out space javascript-typing-tutor Kalen Cobb

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -9,13 +9,25 @@ $focus.addEventListener('keydown', pressedKey);
 var i = 0;
 
 function pressedKey(event) {
+  if (i === 30) {
+    return;
+  }
   console.log('typeOf', typeof event.key);
   console.log('typeOf', typeof $keyPress[i].textContent);
-  console.log('event.key:', event.key);
-  console.log('keyPress:', $keyPress[i].textContent);
-  if (event.key === $keyPress[i].textContent) {
+  console.log('Key Pressed:', event.key);
+  console.log('Expected Key:', $keyPress[i].textContent);
+  if (event.key === ' ' && $keyPress[i].textContent === ' ') {
     $keyPress[i].setAttribute('class', 'correct');
     i += 1;
+    if (i === 30) {
+      return;
+    }
+  } else if (event.key === $keyPress[i].textContent) {
+    $keyPress[i].setAttribute('class', 'correct');
+    i += 1;
+    if (i === 30) {
+      return;
+    }
     $keyPress[i].setAttribute('class', 'underline');
   } else {
     $keyPress[i].setAttribute('class', 'wrong underline');
