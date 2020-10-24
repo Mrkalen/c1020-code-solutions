27854cd752205e1b381232e884350900f1d87e6b
Mrkalen <mrkalen@hotmail.com>
2020-10-23 18:01:45 -0700

    in progress accuracy javascript-typing-tutor Kalen Cobb

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -3,7 +3,9 @@ var count = 0;
 var $keyPress = document.querySelectorAll('span');
 
 var $focus = document.querySelector('body');
-
+console.log('focus:', $focus);
+var $accuracyCounter = document.querySelector('.counter');
+console.log('accuracy counter:', $accuracyCounter);
 $focus.addEventListener('keydown', pressedKey);
 
 var i = 0;
@@ -12,17 +14,7 @@ function pressedKey(event) {
   if (i === 30) {
     return;
   }
-  console.log('typeOf', typeof event.key);
-  console.log('typeOf', typeof $keyPress[i].textContent);
-  console.log('Key Pressed:', event.key);
-  console.log('Expected Key:', $keyPress[i].textContent);
-  if (event.key === '  ' && $keyPress[i].textContent === ' ') {
-    $keyPress[i].setAttribute('class', 'correct');
-    i += 1;
-    if (i === 30) {
-      return;
-    }
-  } else if (event.key === $keyPress[i].textContent) {
+  if (event.key === $keyPress[i].textContent) {
     $keyPress[i].setAttribute('class', 'correct');
     i += 1;
     if (i === 30) {
@@ -34,6 +26,7 @@ function pressedKey(event) {
   }
   if (event.key) {
     count++;
-    console.log('count:', count);
   }
+  var accuracy = Math.floor(100 / (count / i));
+
 }
