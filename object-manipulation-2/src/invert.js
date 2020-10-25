c6b17cfafd462b1878a693c9da4cc7ac6d9c35a1
Mrkalen <mrkalen@hotmail.com>
2020-10-25 12:36:54 -0700

    completed object-manipulation-2 Kalen Cobb

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,16 @@
 /* eslint-disable no-unused-vars */
+function invert(source) {
+  var properties = [];
+  var value = [];
+  var inverted = {};
+
+  for (var property in source) {
+    value.push(source[property]);
+    properties.push(property);
+  }
+
+  for (var i = 0; i < value.length; i++) {
+    inverted[value[i]] = properties[i];
+  }
+  return inverted;
+}
--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+
+function omit(source, keys) {
+  var omited = {};
+  for (var property in source) {
+    omited[property] = source[property];
+  }
+  for (var i = 0; i < keys.length; i++) {
+    delete omited[keys[i]];
+  }
+  return omited;
+}
