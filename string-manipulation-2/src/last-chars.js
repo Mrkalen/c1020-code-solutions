567fb34194e7c4cb676e5d52ba638eaa0284de14
Mrkalen <mrkalen@hotmail.com>
2020-11-02 08:49:45 -0800

    finished lastChars string-manipulation-2 Kalen Cobb

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -2,3 +2,18 @@
 function lastChars(length, string) {
   var chars = string.split('');
   var newString;
+  for (var i = length; i > 0; i--) {
+    if (newString === undefined) {
+      if (chars.length < length) {
+        return string;
+      } else {
+        newString = chars[chars.length - i];
+      }
+    } else if (newString.length === length) {
+      return newString;
+    } else {
+      newString += chars[chars.length - i];
+    }
+  }
+  return newString;
+}
