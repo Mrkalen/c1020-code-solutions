210089719a26228f673a275a118379d83c8619cb
Mrkalen <mrkalen@hotmail.com>
2020-11-06 09:56:25 -0800

    in progress reverseWords for string-manipulation-3

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+function reverseWords(string) {
+  debugger;
+  var words = [];
+  var reversed = [];
+  words = string.split(' ');
+  for (var i = 0; i < words.length; i++) {
+    reversed += words[i].split('').reverse();
+  }
+  return console.log('reversed', reversed);
+}
