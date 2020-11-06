38bece9d5c672d51ddb932f4a4cf9075ef67df31
Mrkalen <mrkalen@hotmail.com>
2020-11-06 12:32:58 -0800

    in progress title-case string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,59 @@
 /* eslint-disable no-unused-vars */
+function titleCase(string) {
+  debugger;
+  var words = string.split(' ');
+  var capitalizedWords = '';
+  for (var i = 0; i < words.length; i++) {
+    if (i === 0) {
+      capitalizedWords = capitalize(words[i]);
+    } else if (words[i].length > 3) {
+      capitalizedWords += ' ' + capitalize(words[i]);
+    }
+  }
+  return capitalizedWords;
+}
+
+// Capitalize word string manipulation 1
+
+function capitalize(word) {
+  word = word.toLowerCase();
+  word = word[0].toUpperCase() + word.slice(1);
+  return word;
+}
+
+// capitalizeWord string manipulation 2
+
+function capitalizeWord(word) {
+  word = word.toLowerCase();
+  word = word[0].toUpperCase() + word.slice(1);
+
+  if (word === 'Javascript') {
+    word = word.replace('s', 'S');
+  }
+  return word;
+}
+
+// CapitalizeWords
+
+function capitalizeWords(string) {
+  string = string.toLowerCase();
+
+  var stringSplit = string.split(' ');
+  for (var i = 0; i < stringSplit.length; i++) {
+    var word = [];
+    var newString;
+    word = stringSplit[i];
+    word = word[0].toUpperCase() + word.slice(1);
+
+    if (word === 'Javascript') {
+      word = word.replace('s', 'S');
+      newString = newString + ' ' + word;
+    }
+    if (newString === undefined) {
+      newString = word;
+    } else {
+      newString = newString + ' ' + word;
+    }
+  }
+  return newString;
+}
