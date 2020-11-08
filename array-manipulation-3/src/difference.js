93d2d0bc10b6aa0631c0e57c7fcfe100d0d82d97
MrKalen <mrkalen@hotmail.com>
2020-11-08 15:33:33 -0800

    finished difference in progress on union for array-manipulation-3

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,23 @@
 /* eslint-disable no-unused-vars */
+function difference(first, second) {
+  var difference = [];
+  var i = 0;
+  while (i < first.length) {
+    if (second.includes(first[i])) {
+      i++;
+    } else {
+      difference.push(first[i]);
+      i++;
+    }
+  }
+  var j = 0;
+  while (j < second.length) {
+    if (first.includes(second[j])) {
+      j++;
+    } else {
+      difference.push(second[j]);
+      j++;
+    }
+  }
+  return difference;
+}
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,7 @@
 /* eslint-disable no-unused-vars */
+// function union(first, second) {
+//   var union = [];
+//   for (var i = 0; i < first.length; i++) {
+
+//   }
+// }
