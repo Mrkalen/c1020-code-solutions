0d91b22fea1c7e070ba8dcd487790f0a0c85fd48
MrKalen <mrkalen@hotmail.com>
2020-11-20 16:32:32 -0800

    completed union for array manipulation 3

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,7 +1,15 @@
 /* eslint-disable no-unused-vars */
-// function union(first, second) {
-//   var union = [];
-//   for (var i = 0; i < first.length; i++) {
-
-//   }
-// }
+function union(first, second) {
+  var union = [];
+  for (var i = 0; i < first.length; i++) {
+    if (union.includes(first[i]) !== true) {
+      union.push(first[i]);
+    }
+  }
+  for (var j = 0; j < second.length; j++) {
+    if (union.includes(second[j]) !== true) {
+      union.push(second[j]);
+    }
+  }
+  return union;
+}
