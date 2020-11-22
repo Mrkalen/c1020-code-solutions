c13606bdede53f987562498b162906074e67dd13
MrKalen <mrkalen@hotmail.com>
2020-11-22 13:58:26 -0800

    Created logic to find the shortest array so that I only needed one loop

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,18 +1,19 @@
 /* eslint-disable no-unused-vars */
 function zip(first, second) {
   var zipped = [];
+  var shortest;
+
   if (first.length === second.length) {
-    for (var i = 0; i < first.length; i++) {
-      zipped.push([first[i], second[i]]);
-    }
+    shortest = first.length;
   } else if (first.length > second.length) {
-    for (var j = 0; j < second.length; j++) {
-      zipped.push([first[j], second[j]]);
-    }
+    shortest = second.length;
   } else {
-    for (var k = 0; k < first.length; k++) {
-      zipped.push([first[k], second[k]]);
-    }
+    shortest = first.length;
   }
+
+  for (var i = 0; i < shortest; i++) {
+    zipped.push([first[i], second[i]]);
+  }
+
   return zipped;
 }
