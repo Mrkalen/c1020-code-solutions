85fc54277c602ce002e391848deaceabd9debd6d
MrKalen <mrkalen@hotmail.com>
2020-11-22 13:34:07 -0800

    collapsed code by inverting conditional expressions

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,22 +1,14 @@
 /* eslint-disable no-unused-vars */
 function difference(first, second) {
   var difference = [];
-  var i = 0;
-  while (i < first.length) {
-    if (second.includes(first[i])) {
-      i++;
-    } else {
+  for (var i = 0; i < first.length; i++) {
+    if (!second.includes(first[i])) {
       difference.push(first[i]);
-      i++;
     }
   }
-  var j = 0;
-  while (j < second.length) {
-    if (first.includes(second[j])) {
-      j++;
-    } else {
+  for (var j = 0; j < second.length; j++) {
+    if (!first.includes(second[j])) {
       difference.push(second[j]);
-      j++;
     }
   }
   return difference;
