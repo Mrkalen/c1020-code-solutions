d122ffab81110b1847b7db6cdd41b5ecb48b33a3
MrKalen <mrkalen@hotmail.com>
2020-11-30 09:33:19 -0800

    in progress figuring out ties

--- working-with-collections/main.js
+++ working-with-collections/main.js
@@ -26,58 +26,58 @@ const players = [
 ];
 
 const cards = [
-  { rank: 'Ace', suit: 'clubs' },
-  { rank: 2, suit: 'clubs' },
-  { rank: 3, suit: 'clubs' },
-  { rank: 4, suit: 'clubs' },
-  { rank: 5, suit: 'clubs' },
-  { rank: 6, suit: 'clubs' },
-  { rank: 7, suit: 'clubs' },
-  { rank: 8, suit: 'clubs' },
-  { rank: 9, suit: 'clubs' },
-  { rank: 10, suit: 'clubs' },
-  { rank: 'Jack', suit: 'clubs' },
-  { rank: 'Queen', suit: 'clubs' },
-  { rank: 'King', suit: 'clubs' },
-  { rank: 'Ace', suit: 'diamonds' },
-  { rank: 2, suit: 'diamonds' },
-  { rank: 3, suit: 'diamonds' },
-  { rank: 4, suit: 'diamonds' },
-  { rank: 5, suit: 'diamonds' },
-  { rank: 6, suit: 'diamonds' },
-  { rank: 7, suit: 'diamonds' },
-  { rank: 8, suit: 'diamonds' },
-  { rank: 9, suit: 'diamonds' },
-  { rank: 10, suit: 'diamonds' },
-  { rank: 'Jack', suit: 'diamonds' },
-  { rank: 'Queen', suit: 'diamonds' },
-  { rank: 'King', suit: 'diamonds' },
-  { rank: 'Ace', suit: 'hearts' },
-  { rank: 2, suit: 'hearts' },
-  { rank: 3, suit: 'hearts' },
-  { rank: 4, suit: 'hearts' },
-  { rank: 5, suit: 'hearts' },
-  { rank: 6, suit: 'hearts' },
-  { rank: 7, suit: 'hearts' },
-  { rank: 8, suit: 'hearts' },
-  { rank: 9, suit: 'hearts' },
-  { rank: 10, suit: 'hearts' },
-  { rank: 'Jack', suit: 'hearts' },
-  { rank: 'Queen', suit: 'hearts' },
-  { rank: 'King', suit: 'hearts' },
-  { rank: 'Ace', suit: 'spades' },
-  { rank: 2, suit: 'spades' },
-  { rank: 3, suit: 'spades' },
-  { rank: 4, suit: 'spades' },
-  { rank: 5, suit: 'spades' },
-  { rank: 6, suit: 'spades' },
-  { rank: 7, suit: 'spades' },
-  { rank: 8, suit: 'spades' },
-  { rank: 9, suit: 'spades' },
-  { rank: 10, suit: 'spades' },
-  { rank: 'Jack', suit: 'spades' },
-  { rank: 'Queen', suit: 'spades' },
-  { rank: 'King', suit: 'spades' }
+  { rank: 'Ace', suit: 'clubs', value: 11 },
+  { rank: 2, suit: 'clubs', value: 2 },
+  { rank: 3, suit: 'clubs', value: 3 },
+  { rank: 4, suit: 'clubs', value: 4 },
+  { rank: 5, suit: 'clubs', value: 5 },
+  { rank: 6, suit: 'clubs', value: 6 },
+  { rank: 7, suit: 'clubs', value: 7 },
+  { rank: 8, suit: 'clubs', value: 8 },
+  { rank: 9, suit: 'clubs', value: 9 },
+  { rank: 10, suit: 'clubs', value: 10 },
+  { rank: 'Jack', suit: 'clubs', value: 10 },
+  { rank: 'Queen', suit: 'clubs', value: 10 },
+  { rank: 'King', suit: 'clubs', value: 10 },
+  { rank: 'Ace', suit: 'diamonds', value: 11 },
+  { rank: 2, suit: 'diamonds', value: 2 },
+  { rank: 3, suit: 'diamonds', value: 3 },
+  { rank: 4, suit: 'diamonds', value: 4 },
+  { rank: 5, suit: 'diamonds', value: 5 },
+  { rank: 6, suit: 'diamonds', value: 6 },
+  { rank: 7, suit: 'diamonds', value: 7 },
+  { rank: 8, suit: 'diamonds', value: 8 },
+  { rank: 9, suit: 'diamonds', value: 9 },
+  { rank: 10, suit: 'diamonds', value: 10 },
+  { rank: 'Jack', suit: 'diamonds', value: 10 },
+  { rank: 'Queen', suit: 'diamonds', value: 10 },
+  { rank: 'King', suit: 'diamonds', value: 10 },
+  { rank: 'Ace', suit: 'hearts', value: 11 },
+  { rank: 2, suit: 'hearts', value: 2 },
+  { rank: 3, suit: 'hearts', value: 3 },
+  { rank: 4, suit: 'hearts', value: 4 },
+  { rank: 5, suit: 'hearts', value: 5 },
+  { rank: 6, suit: 'hearts', value: 6 },
+  { rank: 7, suit: 'hearts', value: 7 },
+  { rank: 8, suit: 'hearts', value: 8 },
+  { rank: 9, suit: 'hearts', value: 9 },
+  { rank: 10, suit: 'hearts', value: 10 },
+  { rank: 'Jack', suit: 'hearts', value: 10 },
+  { rank: 'Queen', suit: 'hearts', value: 10 },
+  { rank: 'King', suit: 'hearts', value: 10 },
+  { rank: 'Ace', suit: 'spades', value: 11 },
+  { rank: 2, suit: 'spades', value: 2 },
+  { rank: 3, suit: 'spades', value: 3 },
+  { rank: 4, suit: 'spades', value: 4 },
+  { rank: 5, suit: 'spades', value: 5 },
+  { rank: 6, suit: 'spades', value: 6 },
+  { rank: 7, suit: 'spades', value: 7 },
+  { rank: 8, suit: 'spades', value: 8 },
+  { rank: 9, suit: 'spades', value: 9 },
+  { rank: 10, suit: 'spades', value: 10 },
+  { rank: 'Jack', suit: 'spades', value: 10 },
+  { rank: 'Queen', suit: 'spades', value: 10 },
+  { rank: 'King', suit: 'spades', value: 10 }
 ];
 
 const shuffled = [];
@@ -87,5 +87,69 @@ function shuffle(deck) {
   while (deck.length !== 0) {
     const card = Math.floor(Math.random() * deck.length);
     shuffled.push(deck[card]);
+    deck.splice(card, 1);
   }
 }
+
+let count = 1;
+
+shuffle(cards);
+
+function deal(deck) {
+  for (let i = 0; i < players.length * 2; i++) {
+    if (count === 1) {
+      players[0].hand.push(shuffled[i]);
+      count += 1;
+    } else if (count === 2) {
+      players[1].hand.push(shuffled[i]);
+      count += 1;
+    } else if (count === 3) {
+      players[2].hand.push(shuffled[i]);
+      count += 1;
+    } else if (count === 4) {
+      players[3].hand.push(shuffled[i]);
+      count = 1;
+    }
+  }
+}
+
+deal(shuffled);
+
+function winnerWinnerChickenDinner() {
+  let bob = 0;
+  let bill = 0;
+  let brad = 0;
+  let benjamin = 0;
+
+  for (let i = 0; i < players.length; i++) {
+    if (i === 0) {
+      bob = addCards(players[i].hand);
+    } else if (i === 1) {
+      bill = addCards(players[i].hand);
+    } else if (i === 2) {
+      brad = addCards(players[i].hand);
+    } else {
+      benjamin = addCards(players[i].hand);
+    }
+  }
+
+  const winner = Math.max(bob, bill, brad, benjamin);
+
+  if (bob === winner) {
+    return 'bob';
+  } else if (bill === winner) {
+    return 'bill';
+  } else if (brad === winner) {
+    return 'brad';
+  } else if (benjamin === winner) {
+    return 'benjamin';
+  }
+}
+
+function addCards(hands) {
+  let score = 0;
+  score = hands[0].value + hands[1].value;
+  return score;
+}
+
+console.log(`${winnerWinnerChickenDinner()} is the winner!!!!`);
