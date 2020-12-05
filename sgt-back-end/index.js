674ab5d3ebb9f2824f2d36c832c4fc2923187ca6
MrKalen <mrkalen@hotmail.com>
2020-12-04 18:12:13 -0800

    in progress making sure post works flawlessly

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -11,7 +11,7 @@ app.get('/api/grades/:gradeId', (req, res, next) => {
   const gradeId = parseInt(req.params.gradeId, 10);
   if (!Number.isInteger(gradeId) || gradeId <= 0) {
     res.status(400).json({ error: 'GradeId must be a positive integer' });
-
+    return;
   }
   const sql = `
     select *
@@ -32,6 +32,35 @@ app.get('/api/grades/:gradeId', (req, res, next) => {
       console.error(err);
       res.status(500).json({ error: 'An unexpected error has occured.' });
     });
+});
+
+app.post('/api/grades', (req, res, next) => {
+  const input = req.body;
+  const name = req.body.name;
+  const score = req.body.score;
+  const course = req.body.course;
+  const scoreNum = parseInt(score, 10);
+  if (!name || !score || !course) {
+    res.status(400).json({ Error: 'Grade must include a name, course, and score' });
+
+  } else if (score < 1 || score > 100 || isNaN(scoreNum)) {
+    res.status(400).json({ Error: 'Score must be a number between 1-100' });
+
+  }
+  const params = [name, course, score];
+  const sql = `
+    insert into "grades" ("name", "course", "score")
+         values ($1, $2, $3)
+  `;
+  db.query(sql, params)
+    .then(result => {
+      res.status(201).json(input);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ Error: 'An unexpected error has occured' });
+    });
+
 });
 
 app.listen(3000, () => {
