c3ab1385aa3ea4b5a01079e13000002b36f3b5e6
MrKalen <mrkalen@hotmail.com>
2020-12-13 16:21:11 -0800

    In progress: testing Cap validation method

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -17,23 +17,55 @@ class ValidatedInput extends React.Component {
     this.setState();
   }
 
+  digitValidation(pass) {
+    const digit = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (let i = 1; i < digit.length; i++) {
+      if (pass.includes(i)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  capValidation(pass) {
+    const caps = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    for (let i = 0; i < caps.length; i++) {
+      if (pass.includes(i)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // passValidation() {
+  //   const message = 'A password is required';
+  //   const errorClass = 'red';
+  //   const passValue = this.state.password;
+  //   // console.log('passValue:', typeof passValue);
+
+  // }
+
   render() {
-    let message = 'A password is required';
     const passValue = this.state.password;
-    // console.log('passValue:', typeof passValue);
+    const errorClass = 'red';
+    let message = 'banana';
     if (passValue === '') {
       message = 'A password is required';
     } else if (passValue.length < 8) {
       message = 'Your password is too short';
+    } else if (!this.digitValidation(passValue)) {
+      console.log('digit failed');
+    } else if (this.capValidation(passValue)) {
+      console.log('Cap failed');
     }
     return (
       <div className='container'>
         <label htmlFor="Password">Password</label>
         <div className='input'>
           <input value={this.state.password} onChange={this.handleChange} type="password" name="password" id="pass" />
-          <i className="fas fa-times"></i>
+          <i className={`fas fa-times ${errorClass}`}></i>
         </div>
-        <p>{message}</p>
+        <p className={`message ${errorClass}`}>{message}</p>
       </div>
     );
   }
